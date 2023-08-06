3fbe6d9f86f0bab3355b5fe95dcf587ca954ed93
ajnash <ajnash.k02@gmail.com>
2023-08-06 11:45:10 +0530

    new updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head title='Ajnash.tech' />
+      <link rel='icon' href='/images/my-logo.ico' />
       <body>
         <Main />
         <NextScript />
